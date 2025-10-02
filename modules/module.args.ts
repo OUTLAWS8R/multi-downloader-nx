d11ca396022ab7b0633ae76108aba33e914fc326
OUTLAWS8R <stratos99kingz@gmail.com>
2025-10-01 23:20:11 -0500

    DKB Customizations

--- modules/module.args.ts
+++ modules/module.args.ts
@@ -1,4 +1,5 @@
-import { dubLanguageCodes, languages, searchLocales, subtitleLanguagesFilter } from './module.langsData';
+import type { LanguageItem } from './module.langsData';
+import { aoSearchLocales, dubLanguageCodes, languages, searchLocales, subtitleLanguagesFilter, allLanguageIdentifiers } from './module.langsData';
 import { CrunchyVideoPlayStreams, CrunchyAudioPlayStreams } from '../@types/enums';
 
 const groups = {
@@ -484,24 +485,31 @@ const args: TAppArg<boolean | number | string | unknown[]>[] = [
 		type: 'boolean',
 		usage: ''
 	},
-	{
-		name: 'dubLang',
-		describe:
-			'Set the language to download: ' +
-			`\nCrunchy Only: ${languages
-				.filter((a) => a.cr_locale)
-				.map((a) => a.code)
-				.join(', ')}`,
-		docDescribe: true,
-		group: 'dl',
-		choices: dubLanguageCodes,
-		default: {
-			default: [dubLanguageCodes.slice(-1)[0]]
-		},
-		service: ['all'],
-		type: 'array',
-		usage: '${dub1} ${dub2}'
-	},
+  {
+    name: 'dubLang',
+    describe: 'Set the language to download by its tag (e.g., ja-JP, en-US)',
+    docDescribe: true,
+    group: 'dl',
+    choices: allLanguageIdentifiers, // This one is fine because its transformer returns a string[]
+    default: {
+      name: 'dubLang',
+      default: [ 'ja-JP' ]
+    },
+    service: ['all'],
+    type: 'array',
+    usage: '${dub1} ${dub2}',
+    transformer: (values: string[]): string[] => {
+      const { findLangByAnyCode } = require('./module.langsData');
+      return values.map(val => {
+        if (val === 'all') return val;
+        const item = findLangByAnyCode(val);
+        if (!item) {
+          throw new Error(`Unable to find language for tag ${val}!`);
+        }
+        return item.code;
+      });
+    }
+  },
 	{
 		name: 'all',
 		describe: 'Used to download all episodes from the show',
@@ -953,44 +961,40 @@ const args: TAppArg<boolean | number | string | unknown[]>[] = [
 			default: []
 		}
 	},
-	{
-		name: 'defaultAudio',
-		describe: `Set the default audio track by language code\nPossible Values: ${languages.map((a) => a.code).join(', ')}`,
-		docDescribe: true,
-		group: 'mux',
-		service: ['all'],
-		type: 'string',
-		usage: '${args}',
-		default: {
-			default: 'eng'
-		},
-		transformer: (val) => {
-			const item = languages.find((a) => a.code === val);
-			if (!item) {
-				throw new Error(`Unable to find language code ${val}!`);
-			}
-			return item;
-		}
-	},
-	{
-		name: 'defaultSub',
-		describe: `Set the default subtitle track by language code\nPossible Values: ${languages.map((a) => a.code).join(', ')}`,
-		docDescribe: true,
-		group: 'mux',
-		service: ['all'],
-		type: 'string',
-		usage: '${args}',
-		default: {
-			default: 'eng'
-		},
-		transformer: (val) => {
-			const item = languages.find((a) => a.code === val);
-			if (!item) {
-				throw new Error(`Unable to find language code ${val}!`);
-			}
-			return item;
-		}
-	},
+  {
+    name: 'defaultAudio',
+    describe: `Set the default audio track by language tag\nPossible Values: ${allLanguageIdentifiers.join(', ')}`,
+    docDescribe: true,
+    group: 'mux',
+    service: ['all'],
+    type: 'string',
+    usage: '${args}',
+    default: {
+      name: 'defaultAudio',
+      default: 'ja-JP'
+    },
+    transformer: (value: string): LanguageItem => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      return require('./module.langsData').findLangByAnyCode(value)!;
+    },
+  },
+  {
+    name: 'defaultSub',
+    describe: `Set the default subtitle track by language tag\nPossible Values: ${allLanguageIdentifiers.join(', ')}`,
+    docDescribe: true,
+    group: 'mux',
+    service: ['all'],
+    type: 'string',
+    usage: '${args}',
+    default: {
+      name: 'defaultSub',
+      default: 'en-US'
+    },
+    transformer: (value: string): LanguageItem => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      return require('./module.langsData').findLangByAnyCode(value)!;
+    },
+  },
 	{
 		name: 'ccTag',
 		describe: 'Used to set the name for subtitles that contain tranlations for none verbal communication (e.g. signs)',
